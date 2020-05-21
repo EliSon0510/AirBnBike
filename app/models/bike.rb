3f201ba7f8bba7667060304d5f2b0963ec9f644d
Phaedon Valasiadis <faival@protonmail.com>
2020-05-21 13:35:03 +0300

    added description

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,6 +1,7 @@
 class Bike < ApplicationRecord
   belongs_to :user
   has_many :rentals, dependent: :destroy
+  # has_one :description
 
   validates :category, inclusion: {in: ["e-bike", "mountain bike", "tandem bike", "children bike", "e-mountain bike", "city bike", "racing bike"], allow_nil: false}
   validates :price, numericality: {only_integer: true}
