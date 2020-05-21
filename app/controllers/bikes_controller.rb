257b0c334f882ced2946aab0b84b3f5ffaac453f
Florence Carlier <florence.carlier.5@gmail.com>
2020-05-21 16:03:55 +0200

    search bar

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -6,6 +6,12 @@ class BikesController < ApplicationController
   def index
     #@bikes = Bike.all
     @bikes = policy_scope(Bike).order(created_at: :desc)
+    if params[:query].present?
+      sql_query = "category ILIKE :query OR location ILIKE :query"
+      @bikes = Bike.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @bikes = Bike.all
+    end
   end
 
   def top
