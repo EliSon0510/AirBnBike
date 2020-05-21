b88c8134cd62b676253d8d5aa14b45e902219276
Florence Carlier <florence.carlier.5@gmail.com>
2020-05-21 13:57:59 +0200

    seed is working

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -7,10 +7,10 @@ class Bike < ApplicationRecord
 
   has_one_attached :photo
 
-  include PgSearch::Model
-  pg_search_scope :search_by_category#_and_localisation,
-    against: [ :category #,:localisation ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
+  # include PgSearch::Model
+  # pg_search_scope :search_by_category_and_localisation,
+  #   against: [ :category ,:localisation ],
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #   }
 end
