d72773e8182cc829c6749ed9a201ec5c0efc985c
Guillaume de Crombrugghe <guillaume.decrombrugghe@hotmail.be>
2020-05-21 14:09:13 +0200

    progress on dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   end
   resources :rentals, only: [:destroy]
-  resources :dashboard, only: [:show]
+  get 'dashboard', to: 'pages#dashboard'
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
