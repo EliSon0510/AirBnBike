13f1b0c54e6b37b769bb57b0ce232e0c4cc2f10b
Guillaume de Crombrugghe <guillaume.decrombrugghe@hotmail.be>
2020-05-21 18:32:04 +0200

    last pull of thursday

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,6 @@ class PagesController < ApplicationController
     current_date = Time.now
     rentals = Rental.where(user: current_user)
     @past_rentals = rentals.select { |rental| rental.end_date < Time.now }
-    @future_rentals = rentals.select { |rental| rental.start_date > Time.now }
+    @future_rentals = rentals.select { |rental| rental.start_date >= Time.now }
   end
 end
