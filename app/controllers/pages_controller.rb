1b592252c4683e09183e8d0b1ce025ac162cb22e
Guillaume de Crombrugghe <guillaume.decrombrugghe@hotmail.be>
2020-05-22 11:32:12 +0200

    working bookings before adding delete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,10 @@ class PagesController < ApplicationController
     current_date = Time.now
     rentals = Rental.where(user: current_user)
     @past_rentals = rentals.select { |rental| rental.end_date < Time.now }
-    @future_rentals = rentals.select { |rental| rental.start_date >= Time.now }
+    @future_rentals = rentals.select { |rental| rental.start_date > Time.now || rental.start_date == Time.now}
+  end
+
+  def destroy
+
   end
 end
